68ac4eb2d3cb6d87862a71c43a0c194effd860d3
Thomas BUREAU DU COLOMBIER <sio.bureauducolombier@gmail.com>
2023-07-12 13:42:27 +0200

    Edit router and add gitignore

--- Router/Route.js
+++ Router/Route.js
@@ -1,9 +1,8 @@
 export default class Route {
-    constructor(url, title, pathHtml, pathJS = "", pathCSS = "") {
+    constructor(url, title, pathHtml, pathJS = "") {
       this.url = url;
       this.title = title;
       this.pathHtml = pathHtml;
       this.pathJS = pathJS;
-      this.pathCSS = pathCSS;
     }
 }
