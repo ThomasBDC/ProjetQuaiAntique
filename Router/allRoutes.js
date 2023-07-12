e695e4e6e7ca9a57da57367374d277af9f8fef22
Thomas BUREAU DU COLOMBIER <sio.bureauducolombier@gmail.com>
2023-07-12 13:42:51 +0200

    edit router and add gitignore

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -3,10 +3,8 @@ import Route from "./Route.js";
 //Définir ici vos routes
 export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
-    new Route("/galerie", "La galerie", "/pages/galerie.html"),
+    new Route("/galerie", "La galerie", "/pages/galerie.html", "/js/galerie.js"),
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
-
-
 export const websiteName = "Quai Antique";
--- Router/router.js
+++ Router/router.js
@@ -1,50 +1,64 @@
 import Route from "./Route.js";
-import {allRoutes, websiteName} from "./allRoutes.js";
+import { allRoutes, websiteName } from "./allRoutes.js";
 
+// Création d'une route pour la page 404 (page introuvable)
 const route404 = new Route("404", "Page introuvable", "/pages/404.html");
 
+// Fonction pour récupérer la route correspondant à une URL donnée
 const getRouteByUrl = (url) => {
-    let currentRoute = null;
-    allRoutes.forEach(element => {
-        if(element.url == url){
-            currentRoute = element;
-        }
-    });
-    if(currentRoute != null){
-        return currentRoute;
+  let currentRoute = null;
+  // Parcours de toutes les routes pour trouver la correspondance
+  allRoutes.forEach((element) => {
+    if (element.url == url) {
+      currentRoute = element;
     }
-    else{
-        return route404;
-    }
-}
+  });
+  // Si aucune correspondance n'est trouvée, on retourne la route 404
+  if (currentRoute != null) {
+    return currentRoute;
+  } else {
+    return route404;
+  }
+};
 
+// Fonction pour charger le contenu de la page
 const LoadContentPage = async () => {
-    const path = window.location.pathname;
-    //Ajout de l'html
-    const actualRoute = getRouteByUrl(path);
-    const html = await fetch(actualRoute.pathHtml).then((data) => data.text());
-    document.getElementById("main-page").innerHTML = html;
+  const path = window.location.pathname;
+  // Récupération de l'URL actuelle
+  const actualRoute = getRouteByUrl(path);
+  // Récupération du contenu HTML de la route
+  const html = await fetch(actualRoute.pathHtml).then((data) => data.text());
+  // Ajout du contenu HTML à l'élément avec l'ID "main-page"
+  document.getElementById("main-page").innerHTML = html;
 
-    //Ajout du js
-    if(actualRoute.pathJS != ""){
-        var scriptTag = document.createElement('script');
-        scriptTag.setAttribute("type","text/javascript");
-        scriptTag.setAttribute("src", actualRoute.pathJS);
-    
-        document.querySelector("body").appendChild(scriptTag);
-    }
+  // Ajout du contenu JavaScript
+  if (actualRoute.pathJS != "") {
+    // Création d'une balise script
+    var scriptTag = document.createElement("script");
+    scriptTag.setAttribute("type", "text/javascript");
+    scriptTag.setAttribute("src", actualRoute.pathJS);
 
-    //Change Title
-    document.title = actualRoute.title + " - "+ websiteName;
+    // Ajout de la balise script au corps du document
+    document.querySelector("body").appendChild(scriptTag);
+  }
+
+  // Changement du titre de la page
+  document.title = actualRoute.title + " - " + websiteName;
 };
 
+// Fonction pour gérer les événements de routage (clic sur les liens)
 const routeEvent = (event) => {
-    event = event || window.event;
-    event.preventDefault();
-    window.history.pushState({}, "", event.target.href);
-    LoadContentPage();
+  event = event || window.event;
+  event.preventDefault();
+  // Mise à jour de l'URL dans l'historique du navigateur
+  window.history.pushState({}, "", event.target.href);
+  // Chargement du contenu de la nouvelle page
+  LoadContentPage();
 };
 
+// Gestion de l'événement de retour en arrière dans l'historique du navigateur
 window.onpopstate = LoadContentPage;
+// Assignation de la fonction routeEvent à la propriété route de la fenêtre
 window.route = routeEvent;
+// Chargement du contenu de la page au chargement initial
 LoadContentPage();
