9b34c7d412d7d1c7d80d23080c76b4813b5b5fd1
Thomas BUREAU DU COLOMBIER <sio.bureauducolombier@gmail.com>
2023-07-17 12:20:28 +0200

    Page réserver

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -8,6 +8,7 @@ export const allRoutes = [
     new Route("/signup", "Inscription", "/pages/signup.html"),
     new Route("/account", "Mon compte", "/pages/account.html"),
     new Route("/allBook", "Les réservation", "/pages/book/allBook.html"),
+    new Route("/book", "Réserver", "/pages/book/book.html"),
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
