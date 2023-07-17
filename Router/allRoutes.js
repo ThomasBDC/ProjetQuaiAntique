59abd9d2bafe732c105db7c9a2107c72931fd530
Thomas BUREAU DU COLOMBIER <sio.bureauducolombier@gmail.com>
2023-07-17 11:49:26 +0200

    Page mon compte

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -6,6 +6,7 @@ export const allRoutes = [
     new Route("/galerie", "La galerie", "/pages/galerie.html", "/js/galerie.js"),
     new Route("/login", "Connexion", "/pages/connexion.html"),
     new Route("/signup", "Inscription", "/pages/signup.html"),
+    new Route("/account", "Mon compte", "/pages/account.html"),
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
